5eda292cdf02ae41ceff7f897239d698ef65b95e
Leonardo Eugênio Panceri de Araujo <panceri.leo@gmail.com>
2024-03-10 20:11:37 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AutoCadastroComponent } from './components/auto-cadastro/auto-cadastro.component';
 import { LoginComponent } from './components/login/login.component';
+import { TelaInicialClienteComponent } from './components/tela-inicial-cliente';
 
 export const routes: Routes = [
   { path: 'autocadastro', component: AutoCadastroComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'inicio-cliente', component: TelaInicialClienteComponent, pathMatch: 'full'},
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
