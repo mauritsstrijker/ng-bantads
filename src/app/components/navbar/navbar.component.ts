ee316859c345d07694980ceecb497a4fe05946ad
Ricardo Gomes <rico00gomes@gmail.com>
2024-03-11 01:40:31 -0300

    nav bar para mostrar telas ao professor

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [InputTextModule],
+  imports: [
+    InputTextModule,
+    ButtonModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(
+    private router: Router,
+  ) {}
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
+}
