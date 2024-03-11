be1790c6f7e04266108a42acbf577b80c3e70126
Leonardo Eugênio Panceri de Araujo <panceri.leo@gmail.com>
2024-03-10 21:31:27 -0300

    tela inicial cliente

--- src/app/components/tela-inicial-cliente/tela-inicial-cliente.component.ts
+++ src/app/components/tela-inicial-cliente/tela-inicial-cliente.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 
 @Component({
   selector: 'app-tela-inicial-cliente',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, TableModule],
   templateUrl: './tela-inicial-cliente.component.html',
   styleUrl: './tela-inicial-cliente.component.scss',
 })
