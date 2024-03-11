792bad8b172529d9bfb16640c8821f3a243521e3
Maurits <maurits.strijker@outlook.com.br>
2024-03-10 22:56:06 -0300

    Correçao merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AutoCadastroComponent } from './components/auto-cadastro/auto-cadastro.component';
 import { LoginComponent } from './components/login/login.component';
+import { ConsultaExtratoComponent } from './components/consulta-extrato/consulta-extrato.component';
 
 export const routes: Routes = [
   { path: 'autocadastro', component: AutoCadastroComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-
+  { path: 'extrato', component: ConsultaExtratoComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
