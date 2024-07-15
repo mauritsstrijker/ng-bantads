2cbbccfa1bb44f535432bcfcb1aeb359ecc44090
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2024-07-15 17:28:29 -0300

    Req. de depósito, saque e transferência (#21) Refatoração do front-end dos requisitos listados.

--- src/app/components/cliente/deposito/deposito.component.ts
+++ src/app/components/cliente/deposito/deposito.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { InputNumberModule } from 'primeng/inputnumber';
-import { Button, ButtonModule } from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { InputMaskModule } from 'primeng/inputmask';
 import { DialogModule } from 'primeng/dialog';
 import { FormsModule } from '@angular/forms';
 import { CurrencyPipe } from '@angular/common';
@@ -14,13 +13,12 @@ import { CurrencyPipe } from '@angular/common';
     InputNumberModule,
     ButtonModule,
     CardModule,
-    InputMaskModule,
     DialogModule,
     FormsModule,
     CurrencyPipe,
   ],
   templateUrl: './deposito.component.html',
-  styleUrl: './deposito.component.scss',
+  styleUrls: ['./deposito.component.scss'],
 })
 export class DepositoComponent {
   visible = false;
@@ -34,11 +32,11 @@ export class DepositoComponent {
 
   depositar() {
     if (this.valorDeposito !== undefined && this.valorDeposito > 0) {
-      console.log(`Transferindo R$ ${this.valorDeposito.toFixed(2)}`);
+      console.log(`Depositando R$ ${this.valorDeposito.toFixed(2)}`);
       this.valorDeposito = undefined;
       this.visible = false;
     } else {
-      console.error('O valor de depósito não é válido.');
+      console.error('O valor do depósito não é válido.');
     }
   }
 }
--- src/app/components/gerente/consultar-melhores-clientes/consultar-melhores-clientes.component.ts
+++ src/app/components/gerente/consultar-melhores-clientes/consultar-melhores-clientes.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
-import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { InputMaskModule } from 'primeng/inputmask';
 import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-consultar-melhores-clientes',
   standalone: true,
-  imports: [
-    InputNumberModule,
-    ButtonModule,
-    CardModule,
-    InputMaskModule,
-    CalendarModule,
-  ],
+  imports: [ButtonModule, CardModule, CalendarModule],
   templateUrl: './consultar-melhores-clientes.component.html',
-  styleUrl: './consultar-melhores-clientes.component.scss',
+  styleUrls: ['./consultar-melhores-clientes.component.scss'],
 })
 export class ConsultarMelhoresClientesComponent {}
